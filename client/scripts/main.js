2a80e901577a5dea29a1b778f25d1b7eacefa34f
Denis Rotanov <mapholameth@gmail.com>
2014-03-09 07:39:47 +1100

    Fix labels in html. Auto fill location when selecting a server.

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -3,20 +3,28 @@ require(["jquery", "authorization", "test"], function($, auth, test) {
     $("#register").click(function() {
         auth.jsonHandle("register", auth.registerCallback);
     });
+
     $("#login").click(function() {
         auth.jsonHandle("login", auth.loginCallback);
     });
+
     $("#test").click(function() {
         $("#content").hide();
         test.runTests();
         test.clearDB();
     });
+
     $("#logout").click(function() {
         auth.jsonHandle("logout", auth.logoutCallback);
     });
+
     $("#Ok").click(function() {
         var url = document.getElementById("href");
         window.location = url.value;
     })
+
+    $(document).ready(function(){
+        $("#href").attr("value", location.origin);
+    });
 
 });
