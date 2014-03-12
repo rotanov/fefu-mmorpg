926506f23568d46979999ff3d9b167a1d9a5dbb0
Denis Rotanov <mapholameth@gmail.com>
2014-03-12 12:24:59 +1100

    Call ws.startGame at login. Format according to code style.

--- client/scripts/authorization.js
+++ client/scripts/authorization.js
@@ -1,6 +1,9 @@
-define(["jquery", "utils", "ws", "games"], function ($, utils, ws, games) {
+define(["jquery", "utils", "ws", "game"],
+function ($, utils, ws, game) {
+
     var sid;
     var wsUri;
+    var id;
 
     function registerCallback(data) {
 
@@ -10,46 +13,48 @@ define(["jquery", "utils", "ws", "games"], function ($, utils, ws, games) {
         {
             serverAnswer.text("Data is null, request might be failed.").css("color", "red");
 
-        } else if (data.result == "ok"){
+        } else if (data.result === "ok") {
             serverAnswer.text("Registration is successful.").css("color", "green");
 
-        } else if (data.result == "loginExists") {
+        } else if (data.result === "loginExists") {
             serverAnswer.text("This login already exists.");
 
-        } else if (data.result == "badLogin") {
-            serverAnswer.text("Login: minimal length is 2 symbols and " +
-                                    "maximum length is 36 symbols. Allowed charset is " +
-                                    "latin symbols and numbers.").css("color", "red");
+        } else if (data.result === "badLogin") {
+            serverAnswer.text("Login: minimal length is 2 symbols and "
+                + "maximum length is 36 symbols. Allowed charset is "
+                + "latin symbols and numbers.").css("color", "red");
 
-        } else if (data.result == "badPassword") {
-            serverAnswer.text("Password: minimal length is 6 symbols and " +
-                                    "maximum length is 36 symbols. Any character is "+
-                                    "allowed except white space and TAB").css("color", "red");
+        } else if (data.result === "badPassword") {
+            serverAnswer.text("Password: minimal length is 6 symbols and "
+                + "maximum length is 36 symbols. Any character is "
+                + "allowed except white space and TAB").css("color", "red");
         }
     }
 
     function loginCallback(data) {
-        if(data.result == "ok") {
+        if(data.result === "ok") {
             $("#server-answer").text("Authentication is successful.").css("color", "green");
             $("#logout").css("visibility", "visible");
+            $("#formregister").hide();
 
-        } else if (data.result == "invalidCredentials") {
+        } else if (data.result === "invalidCredentials") {
             $("#server-answer").text("Invalid login or password.").css("color", "red");
         }
 
         sid = data.sid;
         wsUri = data.webSocket;
         ws.startGame(sid, wsUri);
-        var url = location.href
-        location.href = url.replace(url.substr(url.lastIndexOf("/") + 1), "map.html");
+        game.start();
+        // var url = location.href;
+        // location.href = url.replace(url.substr(url.lastIndexOf("/") + 1), "map.html");
     }
 
     function logoutCallback(data) {
-        if (data.result == "ok") {
+        if (data.result === "ok") {
             $("#server-answer").text("Lets to register or sign in.").css("color", "green");
             $("#logout").css("visibility", "hidden");
 
-        } else if (data.result == "badSid") {
+        } else if (data.result === "badSid") {
             $("#server-answer").text("Invalid session ID.").css("color", "red");
         }
     }
@@ -59,14 +64,14 @@ define(["jquery", "utils", "ws", "games"], function ($, utils, ws, games) {
             var js = {
                 "action": "logout",
                 "sid": sid
-            }
+            };
 
         } else {
             var js = {
                 "action": action,
                 "login": $("#username").val(),
-                "password": $("#password").val(),
-            }
+                "password": $("#password").val()
+            };
         }
 
         utils.postRequest(js, function (response) {
