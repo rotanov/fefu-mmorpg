b3d2ccf2641688ccfdde912002b6b210b8f4d2de
GuraYaroslava <yaroslava_05@mail.ru>
2014-03-25 20:05:30 +1000

    Remove the semicolon. Remove unused modules.

--- client/scripts/utils.js
+++ client/scripts/utils.js
@@ -1,35 +1,35 @@
-define(["jquery", "mocha", "chai"],
-function ($, mocha, chai) {
+define(["jquery"],
+function ($) {
 
     var serverAddress_ = "http://localhost:6543";
 
     function getServerAddress() {
-        return serverAddress_;
+        return serverAddress_
     }
 
     function setServerAddress(serverAddress, onload) {
-        serverAddress_ = serverAddress;
+        serverAddress_ = serverAddress
     }
 
     function postRequest(object, onLoad, sync) {
         sync = sync || false;
-        var xml = new XMLHttpRequest();
+        var xml = new XMLHttpRequest()
 
-        xml.open("POST", serverAddress_, !sync);
+        xml.open("POST", serverAddress_, !sync)
 
         if (!sync) {
-            xml.responseType = "json";
+            xml.responseType = "json"
 
             xml.onreadystatechange = function () {
                 if (xml.readyState === 4) {
                     if (xml.status === 200) {
-                        onLoad(xml.response);
+                        onLoad(xml.response)
 
                     } else {
-                        console.log("postRequest: failure");
+                        console.log("postRequest: failure")
                     }
                 }
-            };
+            }
         }
 
         xml.send(JSON.stringify(object));
@@ -37,11 +37,11 @@ function ($, mocha, chai) {
         if (sync) {
             if(xml.status === 200) {
                 if (onLoad) {
-                    onLoad($.parseJSON(xml.responseText));
+                    onLoad($.parseJSON(xml.responseText))
                 }
 
             } else {
-                console.log("postRequest: failure");
+                console.log("postRequest: failure")
             }
         }
     }
@@ -52,4 +52,4 @@ function ($, mocha, chai) {
         postRequest: postRequest
     }
 
-});
+})
