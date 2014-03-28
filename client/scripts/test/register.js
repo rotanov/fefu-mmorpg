d4d669b28368ad77686a251335c89a65f0414c7a
GuraYaroslava <yaroslava_05@mail.ru>
2014-03-28 20:05:08 +1000

    Add scripts for different test

---
+++ client/scripts/test/register.js
@@ -0,0 +1,141 @@
+define(["jquery", "test/mocha", "test/chai", "utils", "ws"],
+function ($, m, chai, utils, ws) {
+
+    function testRegister(assert) {
+        describe.only("Register", function() {
+            describe.only("Registration", function() {
+                it("should return ok", function() {
+                    assert.equal("ok", utils.serverHandler({
+                        "action": "register",
+                        "login": "IvanPes",
+                        "password": "123456"
+                    }).result)
+                })
+
+                it("should return ok", function() {
+                    assert.equal("ok", utils.serverHandler({
+                        "action": "register",
+                        "login": "Ivan123",
+                        "password": "123456"
+                    }).result)
+                })
+
+                it("should return badPassword [too short]", function() {
+                    assert.equal("badPassword", utils.serverHandler({
+                        "action": "register",
+                        "login": "123Ivan",
+                        "password": "123"
+                    }).result)
+                })
+
+                it("should return badPassword [too long]", function() {
+                    assert.equal("badPassword", utils.serverHandler({
+                        "action": "register",
+                        "login": "00Ivan00",
+                        "password": "0123456789012345678901234567890123456789"
+                    }).result)
+                })
+
+                it("should return badLogin [invalid characters]", function() {
+                    assert.equal("badLogin", utils.serverHandler({
+                        "action": "register",
+                        "login": "  ", 
+                        "password": "0123456"
+                    }).result)
+                })
+
+                it("should return badLogin [invalid characters]", function() {
+                    assert.equal("badLogin", utils.serverHandler({
+                        "action": "register",
+                        "login": "Иван", 
+                        "password": "0123456"
+                    }).result)
+                })
+
+                it("should return badLogin [too short]", function() {
+                    assert.equal("badLogin", utils.serverHandler({
+                        "action": "register",
+                        "login": "A", 
+                        "password": "0123456"
+                    }).result)
+                })
+
+                it("should return badLogin [too long]", function() {
+                    assert.equal("badLogin", utils.serverHandler({
+                        "action": "register",
+                        "login": "AaaaaBbbbbCccccDddddEeeeeFffffGggggHhhhh",
+                        "password": "0123456"
+                    }).result)
+                })
+
+                it("should return loginExists", function() {
+                    assert.equal("loginExists", utils.serverHandler({
+                        "action": "register",
+                        "login": "IvanPes",
+                        "password": "123456"
+                    }).result)
+                })
+            })
+
+            var sid
+            describe("Login", function() {
+                it("should return ok", function() {
+                    data = utils.serverHandler({
+                        "action": "login",
+                        "login": "IvanPes",
+                        "password": "123456"
+                    })
+                    sid = data.sid
+                    wsUri = data.webSocket
+                    assert.equal("ok", data.result)
+                })
+
+                it("should return invalidCredentials"
+                    + "[login does not exist]", function() {
+                    assert.equal("invalidCredentials", utils.serverHandler({
+                        "action": "login",
+                        "login": "Unknown",
+                        "password": "123456"
+                    }).result)
+                })
+
+                it("should return invalidCredentials"
+                    + "[incorrect password]", function() {
+                    assert.equal("invalidCredentials", utils.serverHandler({
+                        "action": "login",
+                        "login": "IvanPes",
+                        "password": "Mumbo-jumbo"
+                    }).result)
+                })
+            })
+
+            describe("Logout", function() {
+                it("should return ok", function() {
+                    assert.equal("ok", utils.serverHandler({
+                        "action": "logout",
+                        "sid": sid
+                    }).result)
+                })
+
+                it("should return badSid", function() {
+                    assert.equal("badSid", utils.serverHandler({
+                        "action": "logout",
+                        "sid": sid
+                    }).result)
+                })
+
+                it("should return badSid", function() {
+                    assert.equal("badSid", utils.serverHandler({
+                        "action": "logout",
+                        "sid": ""
+                    }).result)
+                })
+            })
+        })
+    }
+
+    return {
+        testRegister: testRegister
+    }
+
+})
---
+++ client/scripts/test/websocket.js
@@ -0,0 +1,181 @@
+define(["jquery", "test/mocha", "test/chai", "utils", "ws"],
+function ($, m, chai, utils, ws) {
+
+    function testWebSocket(assert) {
+        describe.only("WebSocket", function(done) {
+            utils.serverHandler({
+                "action": "register",
+                "login": "Pavel",
+                "password": "111111"
+            })
+
+            userData = utils.serverHandler({
+                "action": "login",
+                "login": "Pavel",
+                "password": "111111"
+            })
+
+            before(function(done) {
+                ws.startGame(userData.id, userData.sid, userData.webSocket)
+                setTimeout(done, 200)
+            })
+
+            describe("Dictionary", function() {
+                describe("", function() {
+                    before(function(done) {
+                      ws.dictionary(userData.sid)
+                        setTimeout(done, 200)
+                    })
+
+                    it("should return ok", function(done) {
+                        var data = ws.getDictionary()
+                        assert.equal("ok", data.result)
+                        done()
+                    })
+
+                    it("should have properties ['.', '#']", function(done) {
+                        var data = ws.getDictionary()
+                        assert.property(data.dictionary, ".")
+                        assert.property(data.dictionary, "#")
+                        done()
+                    })
+                })
+
+                before(function(done) {
+                    ws.dictionary("123")
+                    setTimeout(done, 200)
+                })
+
+                it("should return badSid", function(done) {
+                    var data = ws.getDictionary()
+                    assert.equal("badSid", data.result)
+                    done()
+                })
+
+            })
+
+            describe("Look", function() {
+                before(function(done) {
+                    ws.look(userData.sid)
+                    setTimeout(done, 200)
+                })
+
+                it("should return ok", function() {
+                    var response = ws.getLookData()
+                    assert.equal("ok", response.result)
+                })
+
+                it("should have properties [map, actors]", function(done) {
+                    var data = ws.getLookData()
+                    assert.property(data, "map")
+                    assert.property(data, "actors")
+                    done()
+                })
+
+                it("coordinates must be defined", function() {
+                    var data = ws.getLookData()
+                    assert.isDefined(data.x)
+                    assert.isDefined(data.y)
+                })
+
+                describe("", function() {
+                    before(function(done) {
+                        ws.look(".")
+                        setTimeout(done, 200)
+                    })
+
+                    it("should return badSid", function() {
+                        var data = ws.getLookData()
+                        assert.equal("badSid", data.result)
+                    })
+                })
+            })
+
+            describe("Examine", function() {
+                before(function(done) {
+                    ws.examine(userData.id, userData.sid)
+                    setTimeout(done, 200)
+                })
+
+                it("should return ok", function() {
+                    var data = ws.getExamineData()
+                    assert.equal("ok", data.result)
+                })
+
+                it("id must be defined", function() {
+                    var data = ws.getExamineData()
+                    assert.isDefined(data.id)
+                })
+
+                it("type must be player", function() {
+                    var data = ws.getExamineData()
+                    assert.equal("player", data.type)
+                })
+
+                it("login must be defined", function() {
+                    var data = ws.getExamineData()
+                    assert.isDefined(data.login)
+                })
+
+                it("coordinates must be defined", function() {
+                    var data = ws.getExamineData()
+                    assert.isDefined(data.x)
+                    assert.isDefined(data.y)
+                })
+
+                describe("", function() {
+                    before(function(done) {
+                        ws.examine(".", userData.sid)
+                        setTimeout(done, 200)
+                    })
+
+                    it("should return badId", function() {
+                        var data = ws.getExamineData()
+                        assert.equal("badId", data.result)
+                    })
+                })
+
+                describe("", function() {
+                    before(function(done) {
+                        ws.examine(userData.id, ".")
+                        setTimeout(done, 200)
+                    })
+
+                    it("should return badSid", function() {
+                        var data = ws.getExamineData()
+                         assert.equal("badSid", data.result)
+                    })
+                })
+            })
+
+            describe("Move", function() {
+                before(function(done) {
+                    ws.move("west", ws.getTick, userData.sid)
+                    setTimeout(done, 200)
+                })
+
+                it("should return ok", function() {
+                    data = ws.getMoveData()
+                    assert.equal("ok", data.result)
+                })
+
+                describe("", function() {
+                    before(function(done) {
+                        ws.move("west", ws.getTick, ".")
+                        setTimeout(done, 200)
+                    })
+
+                    it("should return badSid", function() {
+                        data = ws.getMoveData()
+                        assert.equal("badSid", data.result)
+                    })
+                })
+            })
+        })
+    }
+
+    return {
+        testWebSocket: testWebSocket
+    }
+
+})
