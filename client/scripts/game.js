e56fb3b5cc6e18970a134cf8247dbd9a82774f96
Darya Gornak <dahin-x@yandex.ru>
2014-03-29 17:48:53 +1100

    Change render map.

--- client/scripts/game.js
+++ client/scripts/game.js
@@ -9,27 +9,30 @@ function (phaser, utils, ws) {
     var leftKey
     var rightKey
     
-    var pressUp = false
-    var pressDown = false
-    var pressLeft = false
-    var pressRight = false
+    var pressUp = 0
+    var pressDown = 0
+    var pressLeft = 0
+    var pressRight = 0
 
     var step = 64
     var gPlayerX
     var gPlayerY
     var actors = [];
+    var count_ver = 0
+    var count_hor = 0
 
     var currWallsPosition = null;
 
     var id_
     var sid_
     var mapGlobal
+    var layer
 
     function Start(id, sid) {
         game = new phaser.Game(
             576, 448,
-           phaser.CANVAS, 
-           "",
+            phaser.AVTO, 
+            "",
             {
                 preload: onPreload,
                 create: onCreate,
@@ -54,7 +57,7 @@ function (phaser, utils, ws) {
         loadMapElem()
         game.load.image("tileset", "assets/tileset1.png")
         game.load.image("player", "assets/player.png")
-        game.load.image("player", "assets/monstr.png")
+        game.load.image("monstr", "assets/monstr.png")
     }
 
     function onCreate() {
@@ -62,7 +65,7 @@ function (phaser, utils, ws) {
 
         mapGlobal.addTilesetImage('tileset');
       
-        var layer = mapGlobal.createLayer('Tile Layer 1');
+        layer = mapGlobal.createLayer('Tile Layer 1');
         
         layer.resizeWorld();
         
@@ -83,11 +86,14 @@ function (phaser, utils, ws) {
     function onUpdate() {
         if (upKey.isDown) {
             if (pressDown){
-                pressDown = false
+                pressDown =false
             } else if ( !pressLeft && !pressRight && !pressUp) {
                 pressUp = true
+                layer._y -= 16
             } else {
                 pressLeft = pressRight = pressUp = false 
+                layer._y += 16 
+                layer._x = 0
                 ws.move("north", ws.getTick(), sid_)
             }
         } else if (downKey.isDown) {
@@ -95,9 +101,13 @@ function (phaser, utils, ws) {
                 pressUp = false
             } else if ( !pressLeft && !pressRight && !pressDown) {
                 pressDown = true
+                layer._y += 16
             } else {
                 pressLeft = pressRight = pressDown = false 
                 ws.move("south", ws.getTick(), sid_)
+                layer._y -= 16
+                layer._x = 0               
+                
             }
         }
 
@@ -106,18 +116,24 @@ function (phaser, utils, ws) {
                 pressRight = false
             } else if ( !pressDown && !pressUp && !pressLeft) {
                 pressLeft = true
+                layer._x -= 16
             } else {
                 pressLeft = pressDown = pressUp = false 
-               ws.move("west", ws.getTick(), sid_)
+                ws.move("west", ws.getTick(), sid_)
+                layer._x += 16
+                layer._y = 0
             }
         } else if (rightKey.isDown) {
              if (pressLeft){
                 pressLeft = false
             } else if (!pressDown && !pressUp && !pressRight) {
                 pressRight = true
+                layer._x += 16 
             } else {
                 pressUp = pressDown = pressRight = false
                 ws.move("east", ws.getTick(), sid_)
+                layer._x -= 16
+                layer._y = 0
             }
         }
 
@@ -147,89 +163,8 @@ function (phaser, utils, ws) {
     function renderWalls(map) {
         for (var i = 0 ; i < map.length; i++) {
             for (var j = 0; j < map[i].length; j++ ) {
-                /* if (pressUp) {
                     if (map[i][j] == "#") {
-                        if (i > 0 && i < map.length-1) {
-                            if (map[i-1][j] == "#" && map[i+1][j] == "#") {
-                                mapGlobal.replace(10, 24, j, i-1, 1, 1) 
-                                mapGlobal.replace(5, 24, j, i, 1, 1) 
-                            } else {
-                                mapGlobal.replace(5, 10, j, i, 1, 1)
-                                mapGlobal.replace(3, 10, j, i, 1, 1)
-                            }
-                        } 
-                    }else if (i == 0) {
-                        if (map[i+1][j] == "#")
-                            mapGlobal.replace(14, 7, j, i, 1, 1)
-                    } else {
-                        mapGlobal.replace(3, 26, j, i, 1, 1)
-                        mapGlobal.replace(12, 28, j, i, 1, 1)
-                        mapGlobal.replace(13, 29, j, i, 1, 1)
-                    }
-                
-                } else if (pressDown) {
-                    if (map[i][j] == "#") {
-                        if (i > 0 && i < map.length-1) {
-                            if (map[i-1][j] == "#" && map[i+1][j] == "#") {
-                                mapGlobal.replace(11, 24, j, i+1, 1, 1) 
-                                mapGlobal.replace(5, 24, j, i, 1, 1) 
-                            } else {
-                                mapGlobal.replace(5, 11, j, i, 1, 1)
-                                mapGlobal.replace(3, 11, j, i, 1, 1)
-                            }
-                        } 
-                    }else if (i == 0) {
-                        if (map[i+1][j] == "#")
-                            mapGlobal.replace(14, 7, j, i, 1, 1)
-                    } else {
-                        mapGlobal.replace(3, 26, j, i, 1, 1)
-                        mapGlobal.replace(12, 28, j, i, 1, 1)
-                        mapGlobal.replace(13, 29, j, i, 1, 1)
-                    }
-                
-                } else if (pressUp) {
-                    if (map[i][j] == "#") {
-                        if (i > 0 && i < map.length-1) {
-                            if (map[i-1][j] == "#" && map[i+1][j] == "#") {
-                                mapGlobal.replace(10, 24, j, i-1, 1, 1) 
-                                mapGlobal.replace(5, 24, j, i, 1, 1) 
-                            } else {
-                                mapGlobal.replace(5, 10, j, i, 1, 1)
-                                mapGlobal.replace(3, 10, j, i, 1, 1)
-                            }
-                        } 
-                    }else if (i == 0) {
-                        if (map[i+1][j] == "#")
-                            mapGlobal.replace(14, 7, j, i, 1, 1)
-                    } else {
-                        mapGlobal.replace(3, 26, j, i, 1, 1)
-                        mapGlobal.replace(12, 28, j, i, 1, 1)
-                        mapGlobal.replace(13, 29, j, i, 1, 1)
-                    }
-                
-                }else if (pressUp) {
-                    if (map[i][j] == "#") {
-                        if (i > 0 && i < map.length-1) {
-                            if (map[i-1][j] == "#" && map[i+1][j] == "#") {
-                                mapGlobal.replace(10, 24, j, i-1, 1, 1) 
-                                mapGlobal.replace(5, 24, j, i, 1, 1) 
-                            } else {
-                                mapGlobal.replace(5, 10, j, i, 1, 1)
-                                mapGlobal.replace(3, 10, j, i, 1, 1)
-                            }
-                        } 
-                    }else if (i == 0) {
-                        if (map[i+1][j] == "#")
-                            mapGlobal.replace(14, 7, j, i, 1, 1)
-                    } else {
-                        mapGlobal.replace(3, 26, j, i, 1, 1)
-                        mapGlobal.replace(12, 28, j, i, 1, 1)
-                        mapGlobal.replace(13, 29, j, i, 1, 1)
-                    }
-                
-                } else {*/
-                    if (map[i][j] == "#") {
-                        mapGlobal.replace(3, 5, j, i, 1, 1)  
+                        mapGlobal.replace(3, 5, j, i, 1, 1)
                         mapGlobal.replace(14, 7, j, i, 1, 1) 
                         mapGlobal.replace(17, 20, j, i, 1, 1) 
                         mapGlobal.replace(19, 23, j, i, 1, 1) 
@@ -238,16 +173,8 @@ function (phaser, utils, ws) {
                         mapGlobal.replace(13, 2, j, i, 1, 1) 
                         mapGlobal.replace(16, 22, j, i, 1, 1)  
                         mapGlobal.replace(18, 21, j, i, 1, 1) 
-                        mapGlobal.replace(10, 5, j, i, 1, 1) 
-                       /*  mapGlobal.replace(24, 5, j, i, 1, 1) 
-                        mapGlobal.replace(11, 5, j, i, 1, 1)  
-                        mapGlobal.replace(26, 5, j, i, 1, 1) 
-                        mapGlobal.replace(28, 14, j, i, 1, 1)
-                        mapGlobal.replace(28, 15, j, i, 1, 1) 
-                        mapGlobal.replace(29, 14, j, i, 1, 1) */
-                        //mapGlobal.replace(29, 15, j, i, 1, 1) 
+
                     } else {
-                        mapGlobal.replace(5, 3, j, i, 1, 1)
                         mapGlobal.replace(7, 14, j, i, 1, 1) 
                         mapGlobal.replace(20, 17, j, i, 1, 1) 
                         mapGlobal.replace(23, 19, j, i, 1, 1) 
@@ -256,18 +183,12 @@ function (phaser, utils, ws) {
                         mapGlobal.replace(2, 13, j, i, 1, 1) 
                         mapGlobal.replace(22, 16, j, i, 1, 1)  
                         mapGlobal.replace(21, 18, j, i, 1, 1) 
-                        mapGlobal.replace(10, 3, j, i, 1, 1) 
-                       /*  mapGlobal.replace(24, 3, j, i, 1, 1)  
-                        mapGlobal.replace(11, 3, j, i, 1, 1)  
-                        mapGlobal.replace(26, 3, j, i, 1, 1) 
-                        mapGlobal.replace(28, 12, j, i, 1, 1)
-                        mapGlobal.replace(29, 13, j, i, 1, 1)  */
-                   
+                        mapGlobal.replace(5, 3, j, i, 1, 1)
                     }
-                //}
             }
         }
     }
+
       
     
     function renderActors(actor) {
