53c83840742d92046d68f28545d3700b8a3c4ca0
Darya Gornak <dahin-x@yandex.ru>
2014-06-29 01:26:37 +1100

    Add Slot

--- server/Player.hpp
+++ server/Player.hpp
@@ -3,6 +3,19 @@
 #include <QString>
 
 #include "Creature.hpp"
+enum Slot
+{
+  left_hand,
+  right_hand,
+  ammo,
+  left_finger,
+  right_finger,
+  neck_amulet,
+  body_armor,
+  head_helmet,
+  forearm_gloves,
+  feet
+};
 
 class Player : public Creature
 {
@@ -16,12 +29,15 @@ public:
   void SetClientTick(const unsigned clientTick);
   virtual QVariantMap atack(Creature* actor);
   void SetBlows();
+  Item* GetSlot(Slot st);
+  void SetSlot(Slot st, Item* item);
   virtual void SetRace();
   QVariantMap Blows;
   // represents creature's inventory
   std::vector<Item*> items_;
+
 private:
   QString login_;
   unsigned clientTick_ = 0;
-
+  QMap<Slot, Item*> slot_;
 };
