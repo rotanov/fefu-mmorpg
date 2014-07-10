b3ce4856bbf8165fcf1dde5501a987adc6ed144e
GuraYaroslava <yaroslava_05@mail.ru>
2014-07-11 04:02:25 +1000

    Merge location.js and websocket.js.

--- client/scripts/test/websocket.js
+++ client/scripts/test/websocket.js
@@ -4,6 +4,14 @@ function($, chai, utils, ws) {
 var tick
 var socket
 var userData
+var dictionary
+
+var playerVelocity
+var slideThreshold
+var ticksPerSecond
+var screenRowCount
+var screenColumnCount
+var pickUpRadius
 
 function testWebSocket() {
     utils.serverHandler({
@@ -35,6 +43,166 @@ function test() {
     var assert = chai.assert
 
     describe.only("WebSocket", function(done) {
+
+        describe("Tick", function() {
+            it("should successfully get tick", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.tick) {
+                        done()
+                   }
+                })
+            })
+        })
+
+        describe("Get Dictionary", function() {
+            it("should return badSid", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data)
+                    if (data.action == "getDictionary") {
+                        assert.equal("badSid", data.result)
+                        done()
+                    }
+                })
+                socket.getDictionary("...")
+            })
+
+            it("should return ok", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "getDictionary") {
+                        assert.equal("ok", data.result)
+                        assert.isDefined(data.dictionary)
+                        var grass = getKey(data.dictionary, "grass")
+                        var wall = getKey(data.dictionary, "wall")
+                        assert.isTrue(grass != undefined)
+                        assert.isTrue(wall != undefined)
+                        dictionary = data.dictionary
+                        done()
+                    }
+                })
+                socket.getDictionary(userData.sid)
+            })
+        })
+
+        describe("Upload map to server", function() {
+            it("should return badMap", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUpMap") {
+                        assert.equal("badMap", data.result, "empty map")
+                        done()
+                    }
+                })
+                socket.setUpMap({"action": "setUpMap", "map": []})
+            })
+
+            it("should return badMap", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUpMap") {
+                        assert.equal("badMap", data.result, "bad symbol")
+                        done()
+                    }
+                })
+                socket.setUpMap({"action": "setUpMap", "map": [["/"]]})
+            })
+
+            it("should return badAction", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUploadMap") {
+                        assert.equal("badAction", data.result, "must setUpMap")
+                        done()
+                    }
+                })
+                var map = [[getKey(dictionary, "grass")]]
+                socket.setUpMap({"action": "setUploadMap", "map": map})
+            })
+
+            it("should return ok", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUpMap") {
+                        assert.equal("ok", data.result)
+                        done()
+                    }
+                })
+                var grass = getKey(dictionary, "grass")
+                var map = [
+                    [grass, grass, grass],
+                    [grass, grass, grass],
+                    [grass, grass, grass]
+                ]
+                socket.setUpMap({"action": "setUpMap", "map": map})
+            })
+        })
+
+        describe("Set Up / Get Constants", function() {
+            it("should return ok", function(done) {
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "getConst") {
+                        assert.equal("ok", data.result)
+                        assert.isDefined(data.playerVelocity)
+                        assert.isDefined(data.slideThreshold)
+                        assert.isDefined(data.ticksPerSecond)
+                        assert.isDefined(data.screenRowCount)
+                        assert.isDefined(data.screenColumnCount)
+                        assert.isDefined(data.pickUpRadius)
+
+                        playerVelocity = data.playerVelocity
+                        slideThreshold = data.slideThreshold
+                        ticksPerSecond = data.ticksPerSecond
+                        screenRowCount = data.screenRowCount
+                        screenColumnCount = data.screenColumnCount
+                        pickUpRadius = data.pickUpRadius
+                        done()
+                    }
+                })
+                socket.getConst()
+            })
+
+            it("should return badAction", function(done) {
+                var data = {
+                    "action": "setUploadConst",
+                    "playerVelocity": 1.0,
+                    "slideThreshold": 0.1,
+                    "ticksPerSecond": 60,
+                    "screenRowCount": 7,
+                    "screenColumnCount": 9,
+                    "pickUpRadius": 1.5,
+                }
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUploadConst") {
+                        assert.equal("badAction", data.result, "must setUpConst")
+                        done()
+                    }
+                })
+                socket.setUpConst(data)
+            })
+
+            it("should return ok", function(done) {
+                var data = {
+                    "action": "setUpConst",
+                    "playerVelocity": playerVelocity,
+                    "slideThreshold": slideThreshold,
+                    "ticksPerSecond": ticksPerSecond,
+                    "screenRowCount": screenRowCount,
+                    "screenColumnCount": screenColumnCount,
+                    "pickUpRadius": pickUpRadius,
+                }
+                socket.setOnMessage(function(e) {
+                    var data = JSON.parse(e.data);
+                    if (data.action == "setUpConst") {
+                        assert.equal("ok", data.result)
+                        done()
+                    }
+                })
+                socket.setUpConst(data)
+            })
+        })
 
         describe("Look", function() {
             it("should return ok", function(done) {
@@ -219,6 +387,24 @@ function test() {
         //socket.setOnMessage(undefined)
     })
     mocha.run()
+}
+
+function getKey(map, value) {
+    var flag = false
+    var keyVal
+    for (key in map) {
+        if (map[key] != value) {
+            continue
+        }
+        flag = true
+        keyVal = key
+        break
+    }
+    if (flag) {
+        return keyVal
+    } else {
+        return false
+    }
 }
 
 return {
