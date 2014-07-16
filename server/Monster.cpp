7b5d685a7bfe23b4a4e68e89ca9d11bc8a76dbe0
Darya Gornak <dahin-x@yandex.ru>
2014-07-16 21:18:51 +1100

    Edit OnCollidenWorld

--- server/Monster.cpp
+++ server/Monster.cpp
@@ -15,8 +15,30 @@ void Monster::OnCollideWorld()
 {
     if (Flags.lastIndexOf("PASS_WALL") == -1)
     {
-        SetDirection(static_cast<EActorDirection>(rand() % 4 + 1));
-        return;
+        auto dir = GetDirection();
+
+        switch (dir)
+        {
+        case EActorDirection::EAST:
+        SetDirection(EActorDirection::SOUTH);
+        break;
+
+        case EActorDirection::WEST:
+        SetDirection(EActorDirection::NORTH);
+        break;
+
+        case EActorDirection::SOUTH:
+        SetDirection(EActorDirection::WEST);
+        break;
+
+        case EActorDirection::NORTH:
+        SetDirection(EActorDirection::EAST);
+        break;
+
+        case EActorDirection::NONE:
+        SetDirection(EActorDirection::NORTH);
+        break;
+        }
     }
 }
 
